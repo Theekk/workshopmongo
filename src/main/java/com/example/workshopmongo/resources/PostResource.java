e429ee65dc6d5b5ff20fca086c09a4f644a0ba7c
Pedro Ignacio <tk-pedro@hotmail.com>
2022-02-09 17:37:03 -0300

    feat: multiple consults

--- src/main/java/com/example/workshopmongo/resources/PostResource.java
+++ src/main/java/com/example/workshopmongo/resources/PostResource.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -30,4 +31,17 @@ public class PostResource {
         List<Post> obj = postService.findByTitle(text);
         return ResponseEntity.ok().body(obj);
     }
+
+    @GetMapping(value = "/fullsearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value = "text",defaultValue = "")String text,
+            @RequestParam(value = "minDate",defaultValue = "")String minDate,
+            @RequestParam(value = "maxDate",defaultValue = "")String maxDate) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = postService.fullSearch(text,min,max);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
